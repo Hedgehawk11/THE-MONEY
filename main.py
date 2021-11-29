3fb187e6ce62a734ed3882641da2725958b49c5e
dbates <84346970+dbates314@users.noreply.github.com>
2021-11-29 18:05:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import time as t
+import random as r
+import sys as sus
+
+cash = 5
+
+
+
+print ('THE MONEY(now with stock)')
+t.sleep(5)
+while True:
+  print('you have $'+str(cash))
+
+  
